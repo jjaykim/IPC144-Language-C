a5b6c610a979d8f916112b1c1635f17320ed597f
jjaykim <dnglglmm@gmail.com>
2020-11-23 01:29:47 -0500

    Week1

---
+++ W01/W01_home.c
@@ -0,0 +1,18 @@
+/*	Name		: Jungjoo Kim
+	ID			: 162 641 195
+	Email		: jkim594@myseneca.ca
+	Section		: IPC 144 NNN
+	Date		: Sep. 15, 2020
+	Description : Workshop 1 - Part-2
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("********************************\n");
+	printf("*** Welcome to C Programming ***\n");
+	printf("********************************\n");
+
+	return 0;
+}
---
+++ W01/W01_lab.c
@@ -0,0 +1,16 @@
+/*	Name		: Jungjoo Kim
+	ID			: 162 641 195
+	Email		: jkim594@myseneca.ca
+	Section		: IPC 144 NNN
+	Date		: Sep. 15, 2020
+	Description : Workshop 1 - Part-1
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("*** Welcome to C Programming ***\n");
+
+	return 0;
+}
