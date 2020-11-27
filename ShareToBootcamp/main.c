8b6412fdf8b30299bf29978e2dd6b44e1452f7b8
jjaykim <dnglglmm@gmail.com>
2020-11-27 01:48:08 -0500

    "ADD GetTenInt"

---
+++ ShareToBootcamp/main.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include "contactHelpers.h"
+#include "contacts.h"
+
+
+
+int main(void)
+{
+  int needInput = 1;
+  char phoneNum[] = { 0 };
+  int i, valid;
+
+  while (needInput == 1)
+  {
+    valid = 0;
+
+    printf("enter the number");
+    scanf("%s", phoneNum);
+    clearKeyboard();
+
+    // (String Length Function: validate entry of 10 characters)
+    for (i = 0; i < 10; i++)
+    {
+      if (!isdigit(phoneNum[i]))
+      {
+        valid++;
+      }
+    }
+
+    if (strlen(&phoneNum) == 10 && valid == 0)
+    {
+        needInput = 0;
+    }
+    else
+    {
+      printf("Enter a 10-digit phone number: ");
+    }
+  }
+    printf("the number is %s", phoneNum);
+
+  return 0;
+}
