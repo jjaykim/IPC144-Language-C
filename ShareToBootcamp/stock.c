3c3e4ad0fdf025c8ec73c41e25638d10304abf5e
jjaykim <dnglglmm@gmail.com>
2020-12-02 22:04:54 -0500

    trying to make inputing data from user and displying error message

--- ShareToBootcamp/stock.c
+++ ShareToBootcamp/stock.c
@@ -32,12 +32,12 @@ int readStockItems(struct StockRecord stockRecord[], int max, int bonus)
                                &stockRecord[i].salesRecord.price, &stockRecord[i].salesRecord.byWeight,
                                stockRecord[i].product);
 
-    while (categoryflag = 1)
+    while (categoryflag == 1)
     {
       if (stockRecord[i].salesRecord.category < MIN_CATEGORY || stockRecord[i].salesRecord.category > MAX_CATEGORY)
       {
         printf("Invalid Category - Enter a number between %d and %d:", MAX_CATEGORY, MIN_CATEGORY);
-        scanf("&d,%.2lf,%d,%s", &stockRecord[i].salesRecord.category, &stockRecord[i].salesRecord.price,
+        scanf("%d,%.2lf,%d,%s", &stockRecord[i].salesRecord.category, &stockRecord[i].salesRecord.price,
               &stockRecord[i].salesRecord.byWeight, stockRecord[i].product);
       }
       else
@@ -46,20 +46,18 @@ int readStockItems(struct StockRecord stockRecord[], int max, int bonus)
       }
     }
 
-    categoryflag = 1;
-
-    while (categoryflag = 1)
+    while (categoryflag == 1)
     {
       if (stockRecord[i].salesRecord.byWeight < 0 || stockRecord[i].salesRecord.byWeight > 1)
       {
-        pritnf("Invalid soldByWeight - Enter a number between 0 and 1:");
+        pritnf("Invalid soldByWeight - Enter a number:");
         scanf("%d,%s", stockRecord[i].salesRecord.byWeight, stockRecord[i].product);
       }
+      else
+      {
+        categoryflag = 0;
+      }
     }
-    
-    
   }
-
-
   return 1;
 };
